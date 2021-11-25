4316e0d3c778baf0c7e3554ff1bd9009d80b1807
luirusso <luirusso_@hotmail.it>
2021-11-25 15:26:51 +0100

    project setup, added axios and base structure

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <img src="../assets/logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+name: 'Header',
+
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    padding: 1rem 10px;
+    display: flex;
+    align-items: center;
+    height: 65px;
+    background-color: #2E3A46;
+    img {
+        max-width: 48px;
+    }
+}
+</style>
